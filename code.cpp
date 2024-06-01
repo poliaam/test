e921a1ebf00def0f6456a78ab721a8eb7fe75635
poliaam <167896158+poliaam@users.noreply.github.com>
2024-06-01 22:41:37 +0300

    code.cpp

--- code.cpp
+++ code.cpp
@@ -1,8 +1,8 @@
 vector<int> incrvec(vector<int> c,int n) {
-  vector<int> c;
-  int n;
-  for (int i = 0; i < c.size(); i++) {
-    c[i] += n;
-  }
-  return c;
+	vector<int> c;
+	int n;
+	for (int i = 0; i < c.size(); i++) {
+		c[i] += n;
+	}
+	return c;
 }
